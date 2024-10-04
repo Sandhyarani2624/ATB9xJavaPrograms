4695150f2655dde207c98442ce21fe5fe6eb46e0
Sandhyarani A <sandhyarani.a97@gmail.com>
2024-10-04 07:06:19 +0530

    Add all the files

---
+++ src/Oct/ex_03102024_tasks/Lab003.java
@@ -0,0 +1,19 @@
+package Oct.ex_03102024_tasks;
+
+public class Lab003 {
+    public static void main(String[] args){
+        System.out.println("Simple Calculator");
+
+        System.out.println("Addition of two numbers");
+        System.out.println("10+20=30");
+
+        System.out.println("Substraction of two numbers");
+        System.out.println("30-20=10");
+
+        System.out.println("Multiplication of two numbers");
+        System.out.println("10*10=100");
+
+        System.out.println("Division of two numbers");
+        System.out.println("20/15=1.33");
+    }
+}
