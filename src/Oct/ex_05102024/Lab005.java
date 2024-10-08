994f3e49d6812512b42f7073b43f4a2b8d469233
Sandhyarani A <sandhyarani.a97@gmail.com>
2024-10-08 06:57:23 +0530

    Add all the files

---
+++ src/Oct/ex_05102024/Lab005.java
@@ -0,0 +1,12 @@
+package Oct.ex_05102024;
+
+public class Lab005 {
+    public static void main(String[] args){
+        int age = 65;
+        //int -> Data type
+        //age -> Variable/Identifier
+        //= -> assignment operator
+        //65 -> Variable value
+        System.out.println(age);
+    }
+}
