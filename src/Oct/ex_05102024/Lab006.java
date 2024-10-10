6b76db070c7b8f98d46a42ff8646e0a6c2b9076a
Sandhyarani A <sandhyarani.a97@gmail.com>
2024-10-10 20:03:16 +0530

    Add all the 9 files

--- src/Oct/ex_05102024/Lab006.java
+++
@@ -1,4 +0,0 @@
-package Oct.ex_05102024;
-
-public class Lab006 {
-}
---
+++ src/Oct/ex_09102024/Lab006.java
@@ -0,0 +1,10 @@
+package Oct.ex_09102024;
+
+public class Lab006 {
+    public static void main(String[] args){
+        char c = 'A';
+        System.out.println(c);
+        String s = "Sandhya";
+        System.out.println(s);
+    }
+}
