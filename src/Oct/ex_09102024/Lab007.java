39220c9d9b2102bd4b6fc92a35c33b385bba1d93
Sandhyarani A <sandhyarani.a97@gmail.com>
2024-10-10 21:48:31 +0530

    Add all the files

---
+++ src/Oct/ex_09102024/Lab007.java
@@ -0,0 +1,11 @@
+package Oct.ex_09102024;
+
+public class Lab007 {
+    public static void main(String[] args){
+        int age = 27;
+        System.out.printf("Your age is %d", age);
+
+        String name = "Sandhya";
+        System.out.printf("Your name is %s", name);
+    }
+}
